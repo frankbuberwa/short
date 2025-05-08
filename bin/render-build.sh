40c95ed993edd83fa179b5c1f380507be51b6b26
Emmanuel Kamala <kamalaemmanuel86@gmail.com>
2025-05-08 16:37:23 +0300

    Edit configuration

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-#rails db:drop RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-rails db:create RAILS_ENV=production
+rails db:drop 
+rails db:create 
 bundle exec rails db:migrate
-bundle exec rails db:seed RAILS_ENV=production
+bundle exec rails db:seed
